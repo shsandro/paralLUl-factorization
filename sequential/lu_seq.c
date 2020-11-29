d94948a34e1a498fb0d407b5a52ca33bfac9ce51
Vinícius Regatieri <viniciusvcr@hotmail.com>
2020-11-29 01:53:47 -0300

    getopt implemented

--- sequential/lu_seq.c
+++ sequential/lu_seq.c
@@ -1,8 +1,8 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-#define N 4
+#include <time.h>
+#include <unistd.h>
 
 int LUPDecompose(double **A, int n, double Tol) {
     int i, j, k, imax;
@@ -37,35 +37,108 @@ int LUPDecompose(double **A, int n, double Tol) {
     return 1;  // decomposition done
 }
 
-int main(int argc, char const *argv[]) {
-    double **A = calloc(N, sizeof(double *));
+void help_menu(const char *prog_name) {
+    printf("Usage: %s [flags]\n", prog_name);
+    printf("    -h               prints this usage guide\n");
+    printf(
+        "    -v               prints the input and output matrix after the "
+        "process\n");
+    printf("    -i <file_name>   sets an input file\n");
+    printf("    -o <file_name>   sets an output file\n");
+    printf(
+        "    -n <number> generate random a matrix of size number x number\n");
 
-    for (size_t i = 0; i < N; i++) A[i] = calloc(N, sizeof(double));
+    printf(
+        "\nIt's important to note that -o option will only work if -v option "
+        "is set\n");
+}
 
-    for (size_t i = 0; i < N; i++) {
-        for (size_t j = 0; j < N; j++) {
-            // A[i][j] = rand() % 10;
-            scanf("%lf", &A[i][j]);
+int main(int argc, char **argv) {
+    int ch, N, n_set = 0, verbose = 0;
+    double **A;
+    FILE *input = NULL, *output = stdout;
+
+    while ((ch = getopt(argc, argv, "i:o:n:hv")) != -1) {
+        switch (ch) {
+            case 'i':
+                input = fopen(optarg, "r");
+                break;
+
+            case 'o':
+                output = fopen(optarg, "w+");
+                break;
+
+            case 'n':
+                N = atoi(optarg);
+                n_set = 1;
+                break;
+
+            case 'h':
+                help_menu(argv[0]);
+                exit(EXIT_SUCCESS);
+
+            case 'v':
+                verbose = 1;
+                break;
+
+            default:
+                help_menu(argv[0]);
+                exit(EXIT_FAILURE);
         }
     }
 
-    printf("Matrix A before:\n");
-    for (size_t i = 0; i < N; i++) {
-        for (size_t j = 0; j < N; j++) {
-            printf("%.2lf ", A[i][j]);
+    if (n_set) {
+        double **A = calloc(N, sizeof(double *));
+
+        for (size_t i = 0; i < N; i++) A[i] = calloc(N, sizeof(double));
+
+        if (input != NULL) {
+            printf("Reading inputs...\n");
+            for (size_t i = 0; i < N; i++) {
+                for (size_t j = 0; j < N; j++) {
+                    fscanf(input, "%lf", &A[i][j]);
+                }
+            }
+        } else {
+            printf("Generating inputs...\n");
+            srand(time(NULL));
+            for (size_t i = 0; i < N; i++) {
+                for (size_t j = 0; j < N; j++) {
+                    A[i][j] = rand() % 100;
+                }
+            }
         }
-        printf("\n");
+
+        if (verbose) {
+            fprintf(output, "\nOriginal matrix:\n");
+            for (size_t i = 0; i < N; i++) {
+                for (size_t j = 0; j < N; j++) {
+                    fprintf(output, "% 6.2lf ", A[i][j]);
+                }
+                fprintf(output, "\n");
+            }
+            printf("\n");
+        }
+
+        printf("Calculating...\n");
+        LUPDecompose(A, N, 0);
+        printf("Done!\n");
+
+        if (verbose) {
+            printf("Writing output...\n");
+            fprintf(output, "\nLU decomposed matrix:\n");
+            for (size_t i = 0; i < N; i++) {
+                for (size_t j = 0; j < N; j++) {
+                    fprintf(output, "% 6.2lf ", A[i][j]);
+                }
+                fprintf(output, "\n");
+            }
+        }
+    } else {
+        printf("Required flag '-N' is not set! Aborting...\n\n");
+        help_menu(argv[0]);
+        exit(EXIT_FAILURE);
     }
 
-    LUPDecompose(A, N, 0);
-
-    printf("\nMatrix A after:\n");
-    for (size_t i = 0; i < N; i++) {
-        for (size_t j = 0; j < N; j++) {
-            printf("%.2lf ", A[i][j]);
-        }
-        printf("\n");
-    }
-
-    return 0;
+    return EXIT_SUCCESS;
 }
